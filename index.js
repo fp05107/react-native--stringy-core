9d2fe67ec56f9480b79cacae117b90df693a7709
subhamPoulimaInfotech <shubham.sahoo@poulimainfo.tech>
2023-12-02 08:40:47 +0530

    test

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function helloNpm() {
-    return "hello NPM"
-  }
-  
-  module.exports = helloNpm
+// index.js
+
+import { Platform } from "react-native";
+
+const reverseString = (str) => {
+  // Add platform-specific logic if needed
+  const reversed = str.split("").reverse().join("");
+  return Platform.OS === "ios" ? reversed : reversed.toUpperCase();
+};
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+module.exports = { reverseString, capitalize };
